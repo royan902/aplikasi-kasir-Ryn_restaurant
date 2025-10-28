f062bb9fda5786a17274738c13cc6bf9d69023ed
royan902 <muhammadroyan701@gmail.com>
2025-10-28 19:41:46 +0700

    MAIN

---
+++ kasir_RYN_coffei/src/kasir_ryn_coffei/Kasir_RYN_coffei.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kasir_ryn_coffei;
+
+/**
+ *
+ * @author Acer Aspire Lite 15
+ */
+public class Kasir_RYN_coffei {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
